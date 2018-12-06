6eabad14463f521bcdd6eec12dbbad0cad170026
Svoykina N <nadya.svojkina@gmail.com>
2018-12-07 01:18:34 +0300

    header management

--- src/components/FlightHeader/index.tsx
+++ src/components/FlightHeader/index.tsx
@@ -1,48 +1,105 @@
 import * as React from 'react';
 
-import {Checkbox} from 'antd';
-import {Input} from 'antd';
+import {Checkbox, Icon, Input} from 'antd';
 
 import './FlightHeader.css';
 
-const Search = Input.Search;
+// const Search = Input.Search;
 
-export default class FlightHeader extends React.Component {
+interface IProps {
+    handleTypeChange: (type: string) => void;
+    handleCheckboxChange: any;
+    handleSearch: any;
+    handleResetSearch: any;
+    type: string;
+}
+
+interface IState {
+    search: string;
+}
+
+export default class FlightHeader extends React.Component<IProps, IState> {
     constructor(props: any) {
         super(props);
+
+        this.state = {
+            search: ''
+        };
+
+        this.onDepartureClick = this.onDepartureClick.bind(this);
+        this.onArrivalClick = this.onArrivalClick.bind(this);
+        this.onChangeSearch = this.onChangeSearch.bind(this);
+        this.onResetSearch = this.onResetSearch.bind(this);
+        this.onSearch = this.onSearch.bind(this);
     }
 
-    public onCheckboxChange() {
-        // tslint:disable-next-line
-        console.log('hi');
+    public onDepartureClick() {
+        if (this.props.type === 'departure') {
+            return;
+        }
+
+        this.props.handleTypeChange('departure');
+    }
+
+    public onArrivalClick() {
+        if (this.props.type === 'arrival') {
+            return;
+        }
+
+        this.props.handleTypeChange('arrival');
+    }
+
+    public onChangeSearch(event: any) {
+        this.setState({ search: event.target.value });
+
+        if (event.target.value === '') {
+            this.onResetSearch();
+        }
+    }
+
+    public onResetSearch() {
+        this.props.handleResetSearch();
+        this.setState({ search: '' });
     }
 
     public onSearch() {
-        // tslint:disable-next-line
-        console.log('hi');
+        this.props.handleSearch(this.state.search);
     }
 
     public render(): JSX.Element {
+        const { search } = this.state;
+        const suffix = search ? <Icon type="close-circle" style={{cursor: 'pointer'}} onClick={this.onResetSearch} /> : null;
+
+        const departureClass = `flight-header__type-item ${this.props.type === 'departure' ? 'active' : ''}`;
+        const arrivalClass = `flight-header__type-item ${this.props.type === 'arrival' ? 'active' : ''}`;
+
         return <div className="flight-header">
             <div className="flight-header__type">
-                <div className="flight-header__type-item active">
+                <div className={departureClass} onClick={this.onDepartureClick}>
                     Вылет
                 </div>
-                <div className="flight-header__type-item">
+                <div className={arrivalClass} onClick={this.onArrivalClick}>
                     Прилет
                 </div>
             </div>
             <div className="flight-header__filters">
-                <Search
+                <Input
                     placeholder="Поиск по номеру рейса"
-                    onSearch={this.onSearch}
-                    style={{ fontSize: '1em', color: '#141251' }}
+                    // placeholder="Поиск по номеру рейса, городу и авиакомпании"
+                    prefix={<Icon type="search" style={{cursor: 'pointer', marginLeft: '-3px'}} onClick={this.onSearch}/>}
+                    suffix={suffix}
+                    value={search}
+                    onChange={this.onChangeSearch}
+                    style={{fontSize: '1em', color: '#141251', marginRight: '1em'}}
                     size="large"
+                    onPressEnter={this.onSearch}
                 />
                 <Checkbox
-                    onChange={this.onCheckboxChange}
-                    style={{color: '#141251', fontSize: '0.9em',
-                        margin: 'auto', width: '20em'}}
+                    onChange={this.props.handleCheckboxChange}
+                    style={{
+                        color: '#141251', fontSize: '0.9em',
+                        margin: 'auto', width: '20em'
+                    }}
                 >
                     Только задержанные
                 </Checkbox>
