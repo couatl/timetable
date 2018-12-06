0046ba8a646106bd5668f293692d1ef516bcf306
Svoykina N <nadya.svojkina@gmail.com>
2018-12-07 01:16:42 +0300

    delete cookie from transport

--- src/service/Transport/index.ts
+++ src/service/Transport/index.ts
@@ -1,6 +1,6 @@
 // import * as fetch from 'isomorphic-fetch';
 
-import { BACK_URL } from './constants';
+import {BACK_URL} from './constants';
 
 class Transport {
     private static instance: any;
@@ -65,55 +65,21 @@ class Transport {
     }
 
     private setRequest(options?: any): any {
-        const cookie = this.getCookie('csrftoken');
-        if (cookie != null) {
-            return {
-                body: options.body,
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                    'X-CSRFToken': `${cookie}`
-                },
-                method: options.method,
-                timeout: options.timeout
-            };
-        } else {
-            return {
-                body: options.body,
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8'
-                },
-                method: options.method,
-                timeout: options.timeout
-            };
-        }
+        return {
+            body: options.body,
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            method: options.method,
+            timeout: options.timeout
+        };
+
     }
-    /*
-        private getFetchRequest(options?: any): any {
-          return this.setRequest(options);
-        }
-      */
+
     private setUpHeaders() {
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
-    }
-
-    private getCookie(name: string) {
-        if (!document.cookie) {
-            return null;
-        }
-
-        const xsrfCookies = document.cookie
-            .split(';')
-            .map(c => c.trim())
-            .filter(c => c.startsWith(name + '='));
-
-        if (xsrfCookies.length === 0) {
-            return null;
-        }
-
-        return decodeURIComponent(xsrfCookies[0].split('=')[1]);
     }
 }
 
